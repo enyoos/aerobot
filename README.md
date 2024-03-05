28698c201078172579268dd5ece6794599ddddb8
enyoos <lebedevhh@outlook.fr>
2024-03-04 21:17:32 -0500

    add, nlp-js, without the 'symlink'

---
+++ README.md
@@ -0,0 +1,34 @@
+#  AreoBot
+
+# Requirements
+- jdk 8
+- mvn ( preferrably the latest )
+- node, npm ( latest LTS )
+
+## Building the xatkit lib
+how to build the xatkit lib (follow the instructions ) : https://github.com/xatkit-bot-platform/xatkit/wiki/Build-Xatkit 
+
+# Installation  
+```console
+git clone https://github.com/enyoos/aerobot.git
+```
+
+```console
+cd areobot
+```
+
+```console
+npm i
+```
+
+```console
+cd src/GreetingsBot
+```
+
+```console
+mvn clean install
+```
+
+```console
+git clone 
+```
--- public/monitor.js
+++ public/monitor.js
@@ -2,42 +2,4 @@
 const observable = document.getElementById('chat');
 
 // TODO : NEED A WAY TO CHANGE THE P TAGS INTO LATEX IN RUNTIME
-
-// const recursive= ( node, pattern ) => {
-//     counter += isNaN(node.childElementCount) ? 0 : node.childElementCount ;
-//     nodes    = node.childNodes;
-
-//     for ( node_ in nodes ) {
-//         console.log ( node_ );
-//         // recursive( node_ );
-//     }
-// } 
-
-
-// observable.addEventListener("DOMSubtreeModified", function() {
-    // const all_msg_box = observable.querySelectorAll('[id=xatkit-message-text]');
-    // let p_tags = observable.getElementsByTagName("p");
-    // console.log ( "the msg box ; " + msg_box );
-    // console.log( all_msg_box );
-    // console.log ( p_tags.length );
-
-
-    // for ( p_tag in p_tags ){
-    //     console.log ( new DOMParser().parseFromString(p_tag, "text/html").innerHTML);
-    // }
-
-    // recursive( observable );
-    // console.log ( "number childs with the p tag : " + counter/*recursive( observable, 0 )*/ );
-    // renderMathInElement(document.getElementById(""), {
-    //     // customised options
-    //     // • auto-render specific keys, e.g.:
-    //     delimiters: [
-    //     {left: '$$', right: '$$', display: true},
-    //     {left: '$', right: '$', display: false},
-    //     {left: '\\(', right: '\\)', display: false},
-    //     {left: '\\[', right: '\\]', display: false},
-    //     ],
-    //     // • rendering keys, e.g.:
-    //     throwOnError : false
-    // });
-// });
+// https://davidrajm.medium.com/using-mathjax-with-dynamic-data-rendering-using-vennila-js-ff426ca48e3b
